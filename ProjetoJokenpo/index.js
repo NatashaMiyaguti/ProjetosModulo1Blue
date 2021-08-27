0290b02a22fa00fb403831f9438a820ba9be239f
Natasha Miyaguti <naty.miyaguti@gmail.com>
2021-08-28 00:19:11 +0200

    atualização

--- ProjetoJokenpo/index.js
+++ ProjetoJokenpo/index.js
@@ -1,18 +1,3 @@
-/*# Esse projeto tem a finalidade de fixar os conhecimentos de Variáveis, console.log, Input 
-# e Condicionais, para isso crie um programa que faça 5 perguntas para uma 
-# pessoa sobre um crime. As perguntas são:
-# • "Você telefonou para a vítima?"
-# • " Você esteve no local do crime?"
-# • " Você mora perto da vítima?"
-# • " Você devia para a vítima?"
-# • " Você já trabalhou com a vítima?" 
-# O programa deve, no final, emitir uma classificação sobre a participação da 
-# pessoa no crime.
-# Se a pessoa responder positivamente a:
-# • 2 questões ela deve ser classificada como "Suspeita",
-# • Entre 3 e 4 como "Cúmplice"
-# • 5 como "Assassino". 
-# • Caso contrário, ele será classificado como "Inocente". */
 
 const color = require("colors"); //colocando cores nas strings
 const prompt = require("prompt-sync")();
